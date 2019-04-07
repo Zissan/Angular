5f1c308eb08dbee1c4ee08530ed59f8632436a40
rockychowdhury <zissan.chowdhury@yahoo.com>
2019-04-07 16:46:42 +0530

    Changes has been done

--- p5a/src/app/profile/profile.component.ts
+++ p5a/src/app/profile/profile.component.ts
@@ -6,7 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  user:any = {
+    name:'Mike Olsen',
+    age:32,
+    designation:'Senior Software Developer',
+    city:'Orlando',
+    image:'/assets/images/profile.png'
+  }
   constructor() { }
 
   ngOnInit() {
