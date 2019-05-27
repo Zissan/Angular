862189fd004b5c782241602d81958d7d9a7783de
zissan <zissan.chowdhury@yahoo.com>
2019-05-27 07:17:34 +0530

    dynamic switching of language

--- localization-demo/src/app/app.component.ts
+++ localization-demo/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'localization-demo';
+
+  languageList = [
+    { code: 'en', label: 'English' },
+    { code: 'es', label: 'Espanol' }
+  ];
+
+  constructor(@Inject(LOCALE_ID) protected localeId: string){}
 }
